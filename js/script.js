98d211014be9f677d140d7bd0b832932e7577742
Pedro Henrique <pedroherubio@gmail.com>
2025-10-25 16:11:04 -0300

    mensagens de erro para o usuario

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
  const form = document.querySelector("form");
 
-  // Função para validar CPF simples (apenas formato)
   function validarCPF(cpf) {
     const re = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
     return re.test(cpf);
   }
 
-  // Função para validar telefone (Brasil)
   function validarTelefone(tel) {
     const re = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
     return re.test(tel);
+  }
+
+  function validarCEP(cep) {
+    const re = /^\d{5}-?\d{3}$/;
+    return re.test(cep);
+  }
+
+  function mostrarErro(input, mensagem) {
+    input.style.border = "2px solid red";
+
+    let erro = input.nextElementSibling;
+    if (erro && erro.classList.contains("erro-msg")) {
+      erro.remove();
+    }
+
+    const span = document.createElement("span");
+    span.classList.add("erro-msg");
+    span.style.color = "red";
+    span.textContent = mensagem;
+    input.insertAdjacentElement("afterend", span);
+  }
+
+  function limparErro(input) {
+    input.style.border = "";
+    let erro = input.nextElementSibling;
+    if (erro && erro.classList.contains("erro-msg")) {
+      erro.remove();
+    }
   }
